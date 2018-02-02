513f152d95dcae988121b7396644b85a643cd700
Alvaro Felipe <alvaro@ed.team>
2018-02-02 00:35:52 -0500

    se importa modales y tabs

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,1 +1,5 @@
 import {menu} from "./modules/menu";
+import {edModal} from "../../node_modules/ed-ui/src/js/modal";
+import {edTabs} from "./modules/tabs";
+window.edModal = edModal;
+window.edTabs = edTabs;
