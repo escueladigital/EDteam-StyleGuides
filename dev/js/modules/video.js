d73d4feb90a4fc9767e5844e01236c28a99b193f
Alvaro Felipe <alvaro@ed.team>
2018-03-11 12:08:57 -0500

    Se corrige funcion para calcular tamaño del video que no recalculaba al redimensionar la ventana

--- dev/js/modules/video.js
+++ dev/js/modules/video.js
@@ -4,10 +4,11 @@ const breakpointVideoSize = matchMedia('(min-aspect-ratio: 8 / 5)')
 const breakpointVideoFixed = matchMedia('(max-width: 960px) and (orientation: portrait)')
 const videoElement = document.getElementById('video-element')
 const videoClass = document.getElementById('video-class')
-const viewportWidth = document.body.getBoundingClientRect().width
+
 
 // define size for video by media query
 export const videoSize = mq => {
+  let viewportWidth = document.body.getBoundingClientRect().width
   let unit = mq.matches
     ? (innerHeight - (11 * 16)) / 9
     : viewportWidth / 16
@@ -19,13 +20,14 @@ export const videoSize = mq => {
 }
 
 const fixedVideo = mq => {
+  let viewportWidth = document.body.getBoundingClientRect().width
   if(videoElement) {
     let e = videoElement.parentElement.nextElementSibling,
         h = (viewportWidth / 16) * 9;
     if (mq.matches) {
       e.style.marginTop = `${h}px`
     } else {
-      e.style.marginTop = 0;
+      e.style.marginTop = '0';
     }
   }
 }
