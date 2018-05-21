620684fc83f882f02ca255982d2f693c74ae7d37
Alvaro Felipe <alvaro@ed.team>
2018-05-21 13:05:22 -0500

    Agrega validaciones en el DOM para evitar errores

--- dev/js/modules/tabs.js
+++ dev/js/modules/tabs.js
@@ -1,32 +1,43 @@
+/*
+* Funcionalidad de tabs
+* */
+
 export const edTabs = () => {
   let container = document.querySelector('.edui-tabs'),
-    tabsContainer = container.querySelector('.tabs'),
-    panelsContainer = container.querySelector('.panels'),
-    tabs = [...tabsContainer.querySelectorAll('.tab')],
-    panels = [...panelsContainer.querySelectorAll('.panel')]
+      tabsContainer, panelsContainer, tabs, panels
 
-  tabs[0].classList.add('active')
-  panels[0].classList.add('active')
-  tabsContainer.setAttribute('data-tab','1')
+    if(container) {
+      tabsContainer = container.querySelector('.tabs')
+      panelsContainer = container.querySelector('.panels')
 
-  tabsContainer.addEventListener('click', e => {
-    let t = e.target,
-      i = tabs.indexOf(t);
-
-    if(t.classList.contains('tab') || t.tagName === "IMG") {
-      tabs.map( tab => tab.classList.remove('active'));
-      panels.map( panel => panel.classList.remove('active'))
-      tabsContainer.removeAttribute('data-tab');
-      tabsContainer.setAttribute('data-tab', `${i+1}`)
-
-      if(t.tagName === 'IMG') {
-        t.parentElement.classList.add('active')
-        i = tabs.indexOf(t.parentElement)
-      } else {
-        t.classList.add('active')
-      }
-
-      panels[i].classList.add('active')
+      if(tabsContainer) tabs = [...tabsContainer.querySelectorAll('.tab')]
+      if(panelsContainer) panels = [...panelsContainer.querySelectorAll('.panel')]
     }
-  })
+
+  if(tabs && tabs.length > 0)  tabs[0].classList.add('active')
+  if(panels && panels.length > 0) panels[0].classList.add('active')
+  if(tabsContainer) {
+    tabsContainer.setAttribute('data-tab','1')
+
+    tabsContainer.addEventListener('click', e => {
+      let t = e.target,
+        i = tabs.indexOf(t);
+
+      if(t.classList.contains('tab') || t.tagName === "IMG") {
+        tabs.map( tab => tab.classList.remove('active'));
+        panels.map( panel => panel.classList.remove('active'))
+        tabsContainer.removeAttribute('data-tab');
+        tabsContainer.setAttribute('data-tab', `${i+1}`)
+
+        if(t.tagName === 'IMG') {
+          t.parentElement.classList.add('active')
+          i = tabs.indexOf(t.parentElement)
+        } else {
+          t.classList.add('active')
+        }
+
+        panels[i].classList.add('active')
+      }
+    })
+  }
 };
