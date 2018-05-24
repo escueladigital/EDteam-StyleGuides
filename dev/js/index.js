f7a8a3535c8c9e58c10527dcf5dc807d0de63a6b
Juan Alexis Mora Angulo <juanalexismoraangulo@hotmail.com>
2018-05-23 18:54:31 -0600

    Sección de curso agregado a la guía de estilos

--- dev/js/index.js
+++ dev/js/index.js
@@ -3,6 +3,7 @@ import {edModal} from "../../node_modules/ed-ui/src/js/modal"
 import {edTabs} from "./modules/tabs"
 import {videoSize} from "./modules/video"
 import {liveLayout} from "./modules/live";
+import {stickyAside} from "./modules/course-aside";
 
 window.edModal = edModal
 window.edTabs = edTabs
