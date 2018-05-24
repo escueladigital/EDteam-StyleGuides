0e3d10a645fa47c59ece76249b19b73a7bc55e67
Juan Alexis Mora Angulo <juanalexismoraangulo@hotmail.com>
2018-05-24 10:11:47 -0600

    Mejora de estilos para versión móvil y modificación de script para el sidebar

--- dev/js/modules/course-aside.js
+++ dev/js/modules/course-aside.js
@@ -1,10 +1,10 @@
-const stickyCardCourse = (referId,cardId) => {
+const stickyCardCourse = (referId, cardId) => {
   const refer = document.getElementById(referId),
-        card = document.getElementById(cardId),
-        largeBp = matchMedia('(min-width: 1024px)')
+    card = document.getElementById(cardId),
+    largeBp = matchMedia('(min-width: 1024px)')
 
   const stickyStyles = breakpointBoolean => {
-    if(refer && card) {
+    if (refer && card) {
       let t = refer.getBoundingClientRect().top,
         l = card.parentElement.getBoundingClientRect().left,
         w = card.parentElement.getBoundingClientRect().width
@@ -14,18 +14,37 @@ const stickyCardCourse = (referId,cardId) => {
         top: ${t}px;
         left: ${l}px;
         width: ${w}px;
-      `
-      : `
-        position: static
+      ` :
+        `
+      position: static
       `
       card.setAttribute('style', styles)
+
+      const temary = document.querySelector(".course-temary");
+
+      addEventListener("scroll", () => {
+        let temaryBottom = temary.getBoundingClientRect().bottom;
+        if (temaryBottom <= card.getBoundingClientRect().bottom) {
+          // El card no debe superar al temario
+          const stylesAbsolute = `
+            position: absolute;
+            top: auto;
+            bottom: 0;
+          `
+          card.setAttribute('style', stylesAbsolute)
+          card.parentElement.style.position = "relative"
+        } 
+      })
+
     }
+
   }
 
   stickyStyles(largeBp.matches)
   addEventListener('resize', () => {
     stickyStyles(largeBp.matches)
   })
+
 }
 
-stickyCardCourse('course-title','course-card')
+stickyCardCourse('course-title', 'course-card')
