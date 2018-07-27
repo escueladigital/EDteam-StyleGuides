5fc358fc9eb4aa3abac115e6ef21c33226558900
Juan Alexis Mora Angulo <juanalexismoraangulo@hotmail.com>
2018-07-27 17:46:12 -0600

    Banner utilizando margin-top

--- dev/js/modules/main-banner.js
+++ dev/js/modules/main-banner.js
@@ -4,24 +4,23 @@ export const mainBanner = (idBanner, classTopRefer) => {
         bannerPlaceholder = document.createElement('div')
 
     if (banner && topRefer) {
-
-        // Función que recibe como parámetro el banner y topRefer
-        // Dicha función de encargará de realizar los cálculos y aplicar estilos para el banner
         const bannerActive = (b, tr) => {
-            // Posicionar banner abajo del nav
-            b.style.top = `${tr.getBoundingClientRect().bottom}px`
-
-            // Aplicamos estilos al bannerPlaceholder
-            bannerPlaceholder.classList.add('banner-placeholder')
-            bannerPlaceholder.setAttribute('style', `height: ${b.getBoundingClientRect().height}px;`)
-            b.after(bannerPlaceholder)
-
-            // Ubicamos el pseudoelemento con variables CSS calculando la altura del banner
-            let t = b.getBoundingClientRect().height - parseInt(getComputedStyle(bannerPlaceholder).getPropertyValue('--height').replace('px', ''));
-            bannerPlaceholder.style.setProperty('--top', `${t}px`)
+            let placeholderHeight = 20,
+                bannerHeight = banner.getBoundingClientRect().height,
+                topBanner = tr.getBoundingClientRect().bottom,
+                styles = `
+                    width: 100%;
+                    height: ${placeholderHeight}px;
+                    margin-top: ${bannerHeight - placeholderHeight}px;
+                    background: var(--body-bg);
+                    clip-path : polygon(0 0, 100% 100%, 100% 100%, 0% 100%);
+                    z-index: var(--z-normal);
+                `
+            b.style.top = `${topBanner}px`
+            bannerPlaceholder.setAttribute('style', styles)
+            banner.after(bannerPlaceholder)
         }
 
-        // Activamos la función
         bannerActive(banner, topRefer)
         addEventListener('resize', () => {
             bannerActive(banner, topRefer)
